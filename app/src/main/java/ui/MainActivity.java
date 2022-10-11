ded708fa015274358755cec5c49857501f4bf776
Kamelia Pezeshki <peze0001@algonquinlive.com>
2022-10-11 14:47:53 -0400

    Lab 2

--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -1,15 +1,19 @@
 package ui;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
-
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import algonquin.cst2335.peze0001.R;
 import algonquin.cst2335.peze0001.databinding.ActivityMainBinding;
 import data.MainViewModel;
 
@@ -17,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     private MainViewModel model;
     private ActivityMainBinding variableBinding;
+    private String selected;
 
 
     @Override
@@ -24,48 +29,72 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
 
-model = new ViewModelProvider(this).get(MainViewModel.class);
+        model = new ViewModelProvider(this).get(MainViewModel.class);
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot());
 
 
-      //  variableBinding.textview.setText(model.editString);
-        //variableBinding.mybutton.setOnClickListener(click ->
-        //{
-          //  model.editString = variableBinding.myedittext.getText().toString();
-            //variableBinding.textview.setText("Your edit text has : " +model.editString)
-        //});
-        variableBinding.mybutton.setOnClickListener(click ->
-        {
+       //  variableBinding.textview.setText(model.editString);
+         variableBinding.mybutton.setOnClickListener(click ->
+         {
             model.editString.postValue(variableBinding.myedittext.getText().toString());
+            variableBinding.textview.setText("Your edit text has : " +model.editString);
+         });
+       variableBinding.mybutton.setOnClickListener(click ->
+       {
+           model.editString.postValue(variableBinding.myedittext.getText().toString());
         });
         model.editString.observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(String s) {
+          @Override
+          public void onChanged(String s) {
 
             }
+       });
+       model.editString.observe( this, s -> {
+
+               variableBinding.textview.setText("Your edit text has "+ s);
+
+
+     });
+    model.variable.observe(this, selected -> {
+                variableBinding.checkbox.setChecked(selected);
+                variableBinding.RadioButton.setChecked(selected);
+                variableBinding.Switch.setChecked(selected);
+                CharSequence text1 = "The value is now: " + selected;
+                int duration1 = Toast.LENGTH_SHORT;
+                Context context = getApplicationContext();
+                Toast toast1 = Toast.makeText(context, text1, duration1);
+                toast1.show();
+
+            });
+
+        variableBinding.checkbox.setOnCheckedChangeListener( (btn, isChecked) -> {
+            model.variable.postValue(isChecked);
+        } );
+        variableBinding.Switch.setOnCheckedChangeListener( (sw, isChecked) -> {
+            model.variable.postValue(isChecked);
+       } );
+        variableBinding.RadioButton.setOnCheckedChangeListener( (rdb, isChecked) -> {
+            model.variable.postValue(isChecked);
+        } );
+
+
+
+
+
+
+
+
+        ImageButton ib = (ImageButton) findViewById(R.id.imageButton);
+        ib.setOnClickListener(v -> {
+
+            CharSequence text1 = "The width :" + v.getWidth() + "the height :" + v.getHeight();
+            int duration1 = Toast.LENGTH_SHORT;
+            Context context = getApplicationContext();
+            Toast toast1 = Toast.makeText(context, text1, duration1);
+            toast1.show();
         });
-        model.editString.observe( this, s -> {
 
-                variableBinding.textview.setText("Your edit text has "+ s);
-
-
-        });
-
-
-        //setContentView(R.layout.activity_main);
-
-      //  TextView mytext = variableBinding.textview;
-      //  final Button btn = variableBinding.mybutton;
-
-        //btn.setOnClickListener(new View.OnClickListener() {
-           // @Override
-            //public void onClick(View view) {
-              //  EditText myedit = variableBinding.myedittext;
-            //    String editString = myedit.getText().toString();
-          //      mytext.setText("Your edit text has: " + editString);
-        //    }
-      //  });//
 
 
     }
