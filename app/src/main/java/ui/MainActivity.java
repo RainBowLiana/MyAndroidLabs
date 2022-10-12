68208f5eb65ead76e037094bad17a91b4a1d130b
Kamelia Pezeshki <peze0001@algonquinlive.com>
2022-10-12 13:55:31 -0400

    Finished Phone Version of Lab 3

--- app/src/main/java/ui/MainActivity.java
+++ app/src/main/java/ui/MainActivity.java
@@ -3,9 +3,14 @@ package ui;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,84 +24,32 @@ import data.MainViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MainViewModel model;
-    private ActivityMainBinding variableBinding;
-    private String selected;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        ImageView imgView;
+        Switch sw;
 
-        model = new ViewModelProvider(this).get(MainViewModel.class);
-        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(variableBinding.getRoot());
+        imgView = findViewById(R.id.imageView2);
+        sw = findViewById(R.id.spin_switch);
 
+        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
 
-       //  variableBinding.textview.setText(model.editString);
-         variableBinding.mybutton.setOnClickListener(click ->
-         {
-            model.editString.postValue(variableBinding.myedittext.getText().toString());
-            variableBinding.textview.setText("Your edit text has : " +model.editString);
-         });
-       variableBinding.mybutton.setOnClickListener(click ->
-       {
-           model.editString.postValue(variableBinding.myedittext.getText().toString());
-        });
-        model.editString.observe(this, new Observer<String>() {
-          @Override
-          public void onChanged(String s) {
-
+                imgView.startAnimation(rotate);
             }
-       });
-       model.editString.observe( this, s -> {
-
-               variableBinding.textview.setText("Your edit text has "+ s);
-
-
-     });
-    model.variable.observe(this, selected -> {
-                variableBinding.checkbox.setChecked(selected);
-                variableBinding.RadioButton.setChecked(selected);
-                variableBinding.Switch.setChecked(selected);
-                CharSequence text1 = "The value is now: " + selected;
-                int duration1 = Toast.LENGTH_SHORT;
-                Context context = getApplicationContext();
-                Toast toast1 = Toast.makeText(context, text1, duration1);
-                toast1.show();
-
-            });
-
-        variableBinding.checkbox.setOnCheckedChangeListener( (btn, isChecked) -> {
-            model.variable.postValue(isChecked);
-        } );
-        variableBinding.Switch.setOnCheckedChangeListener( (sw, isChecked) -> {
-            model.variable.postValue(isChecked);
-       } );
-        variableBinding.RadioButton.setOnCheckedChangeListener( (rdb, isChecked) -> {
-            model.variable.postValue(isChecked);
-        } );
-
-
-
-
-
-
-
-
-        ImageButton ib = (ImageButton) findViewById(R.id.imageButton);
-        ib.setOnClickListener(v -> {
-
-            CharSequence text1 = "The width :" + v.getWidth() + "the height :" + v.getHeight();
-            int duration1 = Toast.LENGTH_SHORT;
-            Context context = getApplicationContext();
-            Toast toast1 = Toast.makeText(context, text1, duration1);
-            toast1.show();
+            else {
+                imgView.clearAnimation();
+            }
         });
-
-
-
     }
+
 
 }
